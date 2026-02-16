21a9ad1267cb9633cbe47541056cde9a122d7b52
Bùi Kiệt <yabiralsei@gmail.com>
2026-02-16 14:10:44 +0700

    bai 2.3

---
+++ Bai2.3/src/giaiptbac1.java
@@ -0,0 +1,15 @@
+
+public class giaiptbac1 {
+
+	public static void main(String[] args) {
+		//khai báo a, b
+		int a = 2, b = 4;
+		int x = -b/a;
+		
+		System.out.println (a + "*x + " + b + "= 0");
+		System.out.println ("-> x = " + x);
+		
+
+	}
+
+}
