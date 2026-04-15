ed5f3d869d15a278c77835b0d6e6d765d3608458
Bùi Kiệt <yabiralsei@gmail.com>
2026-04-15 12:50:46 +0700

    Stream & File Ví dụ 3

---
+++ StreamVaFile_VD3_DocKiTuConsole/src/bhak/ViDuBufferedReader.java
@@ -0,0 +1,21 @@
+package bhak;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ViDuBufferedReader {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str;
+		System.out.println("Nhập chuỗi. Nhập 'stop' để kết thúc: ");
+		do
+		{
+			str = br.readLine();
+			System.out.println(str);
+		} while(!str.equals("stop"));
+
+	}
+
+}
