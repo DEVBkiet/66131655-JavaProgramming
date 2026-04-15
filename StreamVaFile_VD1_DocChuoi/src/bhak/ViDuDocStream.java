9086562ec2da3c84281ef590a88d5e8439948b57
Bùi Kiệt <yabiralsei@gmail.com>
2026-04-15 12:49:52 +0700

    Stream & File Ví dụ 1

---
+++ StreamVaFile_VD1_DocChuoi/src/bhak/ViDuDocStream.java
@@ -0,0 +1,19 @@
+package bhak;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException; 
+
+public class ViDuDocStream {
+
+	public static void main(String[] args) throws IOException {
+		InputStreamReader ipReader = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(ipReader);
+		
+		System.out.print("Nhập một chuỗi bất kỳ: ");
+		String lineDocDuoc = br.readLine();
+		System.out.println("Chuỗi đã nhập: " + lineDocDuoc);
+		
+	}
+
+}
