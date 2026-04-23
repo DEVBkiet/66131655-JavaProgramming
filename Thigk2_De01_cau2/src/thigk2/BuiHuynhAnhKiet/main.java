33280a79ecf99c3ed548388aa145eb3959dca371
Bùi Kiệt <yabiralsei@gmail.com>
2026-04-23 08:10:22 +0700

    Lần thứ 4

--- Thigk2_De01_cau2/src/thigk2/BuiHuynhAnhKiet/main.java
+++ Thigk2_De01_cau2/src/thigk2/BuiHuynhAnhKiet/main.java
@@ -1,5 +1,10 @@
 package thigk2.BuiHuynhAnhKiet;
-
+//Tạo lớp SinhVien gồm: maSV, hoTen, namSinh, diemTB + constructor + getter/setter
+//Trong main: dùng ArrayList lưu danh sách sinh viên
+//Hard-code sẵn 3 sinh viên, in danh sách
+//Nhập thêm 1 sinh viên từ bàn phím, thêm vào list
+//In lại danh sách sau khi thêm
+//Duyệt list, in các sinh viên có tuổi > 20 (tính theo năm hiện tại 2026-năm sinh)
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -20,5 +25,51 @@ public class main {
 		sv2.setHoTen("Tran Thuy Ngoc Tuyen");
 		sv2.setNamSinh(2005);
 		sv2.setDiemTB(8.5);
+		QLSV sv3 = new QLSV();
+		sv3.setMaSV("SV03");
+		sv3.setHoTen("Nguyen Van A");
+		sv3.setNamSinh(2007);
+		sv3.setDiemTB(2.5);
+		
+		ds.add(sv1);
+		ds.add(sv2);
+		ds.add(sv3);
+		
+		System.out.println("Danh sach ban dau:");
+		for (QLSV sv : ds) {
+			System.out.println(sv);
+			System.out.println("-----");
+		}
+	Scanner sc = new Scanner(System.in);
+	
+	QLSV svMoi = new QLSV();
+	
+	System.out.print("Nhap ma SV: ");
+	svMoi.setMaSV(sc.nextLine());
+	
+	System.out.print("Nhap ho ten: ");
+	svMoi.setHoTen(sc.nextLine());
+	
+	System.out.print("Nhap nam sinh: ");
+	svMoi.setNamSinh(sc.nextInt());
+	
+	System.out.print("Nhap diem TB: ");
+	svMoi.setDiemTB(sc.nextDouble());
+	
+	ds.add(svMoi);
+	
+	System.out.println("\nDanh sach sau khi them:");
+	for (QLSV sv : ds) {
+		System.out.println(sv);
+		System.out.println("-----");
 	}
+	
+	System.out.println("\nSinh vien lon hon 20 tuoi:");
+	for (QLSV sv : ds) {
+		if (sv.tinhTuoi() > 20) {
+			System.out.println(sv);
+			System.out.println("-----");
+		}
+	}
+}
 }
